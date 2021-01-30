4c98eafcb6e042581a791aa1d251e151d53147a2
Shiedayn <ali.alperen.uzun@gmail.com>
2021-01-30 14:32:26 +0300

    CarGame Project

---
+++ CarGame/Assets/Scripts/CarScript.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody2D;
+    private float speed;
+    private float rotationSpeed;
+    private int index;
+    public bool isPlayable;
+    private bool endReplay;
+
+    List<PointInTime> pointsInTime;
+    GameManager gameManager;
+    public SpriteRenderer carSprite;
+
+    private void Awake()
+    {
+        carSprite = gameObject.GetComponent<SpriteRenderer>();
+        endReplay = false;
+        isPlayable = false;
+        index = 0;
+        speed = 2f;
+        rotationSpeed = 5f;
+        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+
+        pointsInTime = new List<PointInTime>();
+        gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+    }
+    void Update()
+    {
+        if (gameManager.isTouched)
+        {
+            if (isPlayable)
+            {
+                rigidbody2D.velocity = transform.up * speed;
+                Drive();
+                Record();
+            }
+            else
+            {
+                if (!endReplay)
+                {
+                    Replay();
+                }
+            }
+        }
+    }
+    private void Drive()
+    {
+        //Movement
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.position.x > Screen.width / 2)
+            {
+                rigidbody2D.rotation -= rotationSpeed;
+            }
+            else if (touch.position.x < Screen.width / 2)
+            {
+
+                rigidbody2D.rotation += rotationSpeed;
+            }
+        }
+    }
+
+    //Replay car Position and rotation
+    void Replay()
+    {
+        if (pointsInTime.Count > 0)
+        {
+            if (pointsInTime.Count <= index)
+            {
+                endReplay = true;
+                rigidbody2D.velocity = Vector2.zero;
+            }
+            else
+            {
+                PointInTime pointInTime = pointsInTime[index];
+                transform.position = pointInTime.position;
+                transform.rotation = pointInTime.rotation;
+                index += 1;
+            }
+        }
+    }
+    public void RestartCar(int carNum)
+    {
+        index = 0;
+        if (gameManager.playableCar == carNum)
+        {
+            pointsInTime.Clear();
+        }
+        endReplay = false;
+        rigidbody2D.velocity = Vector2.zero;
+    }
+
+    //Record car Position and rotation
+    void Record()
+    {
+        pointsInTime.Add(new PointInTime(transform.position, transform.rotation));
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Building") || collision.CompareTag("Player"))
+        {
+            gameManager.LoadCheckPoint();
+        }
+        else if (collision.CompareTag("Finish"))
+        {
+            if (isPlayable)
+            {
+                gameManager.Next();
+            }
+        }
+    }
+
+}
---
+++ CarGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject startPosition;
+    public GameObject endPosition;
+    public GameObject car;
+    public int playableCar;
+    public bool isTouched;
+    public GameObject finish;
+    private void Awake()
+    {
+        isTouched = false;
+        playableCar = 0;
+        car.transform.GetChild(playableCar).gameObject.SetActive(true);
+        car.transform.GetChild(playableCar).transform.position = startPosition.transform.GetChild(playableCar).transform.position;
+        car.transform.GetChild(playableCar).transform.rotation = startPosition.transform.GetChild(playableCar).transform.rotation;
+        car.transform.GetChild(playableCar).GetComponent<CarScript>().isPlayable = true;
+
+        finish.transform.position = endPosition.transform.GetChild(playableCar).transform.position;
+    }
+
+    public void LoadCheckPoint()
+    {
+        for (int i = 0; i <= playableCar; i++)
+        {
+            isTouched = false;
+            car.transform.GetChild(i).transform.position = startPosition.transform.GetChild(i).transform.position;
+            car.transform.GetChild(i).transform.rotation = startPosition.transform.GetChild(i).transform.rotation;
+            car.transform.GetChild(i).GetComponent<CarScript>().RestartCar(i);
+        }
+    }
+    private void Update()
+    {
+        if (!isTouched && Input.touchCount > 0)
+        {
+            isTouched = true;
+        }
+    }
+
+    public void Next()
+    {
+        playableCar += 1;
+        if (playableCar > 7)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            finish.transform.position = endPosition.transform.GetChild(playableCar).transform.position;
+            finish.transform.rotation = endPosition.transform.GetChild(playableCar).transform.rotation;
+            car.transform.GetChild(playableCar).gameObject.SetActive(true);
+            LoadCheckPoint();
+            car.transform.GetChild(playableCar).GetComponent<CarScript>().isPlayable = true;
+            car.transform.GetChild(playableCar - 1).GetComponent<CarScript>().isPlayable = false;
+            car.transform.GetChild(playableCar - 1).GetComponent<CarScript>().carSprite.color = Color.red;
+        }
+    }
+}
